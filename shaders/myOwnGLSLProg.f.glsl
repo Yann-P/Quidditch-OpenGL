d40f9875af5b011729eee113f653d336b1b1161b
Pierre Gabon <pierre.gabon@ecole.ensicaen.fr>
2017-03-20 10:19:14 +0100

    ajout des shaders

---
+++ shaders/myOwnGLSLProg.f.glsl
@@ -0,0 +1,14 @@
+#version 410
+
+out vec4 out_color;
+
+void main()
+{
+out_color=vec4(1,1,0,1);
+}
+/*!todo: Create a fragment shader
+  * Lab0: simple version
+  * Lab1: textured version
+  * Lab2: fancier version (shading, cartoon, ...)
+  */
+
