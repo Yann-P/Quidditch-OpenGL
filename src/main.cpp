2c44680458b97af46eaa822c8566b2a06e03975e
Pierre Gabon <pierre.gabon@ecole.ensicaen.fr>
2017-03-20 11:57:56 +0100

    main nettoyé

--- src/main.cpp
+++ src/main.cpp
@@ -80,8 +80,8 @@ void renderFrame(GLuint program, GLuint vao)
   	glfwSwapBuffers(window);
 
   	//--------- Clean state again
-  	//glBindVertexArray(0);
-  	//glUseProgram(0);
+  	glBindVertexArray(0);
+  	glUseProgram(0);
 }
 
 
@@ -100,7 +100,6 @@ void file_contents(string chemin, int* longueur, GLchar * res)
 		res[count] = c;
 		count++;
 	}
-	cout << res << endl;
 	*longueur = count * sizeof(char);
 	fichier.close();
 
@@ -125,6 +124,37 @@ void createIBO(unsigned int* indices,int nbIndices,  GLuint & ibo)
  	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
+void createVAO(GLuint & shaderProgram, GLuint & ibo, GLuint & vbo, GLuint & vao)
+{
+ 	glGenVertexArrays(1,&vao);
+ 	glBindVertexArray( vao);
+ 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+ 	GLint positionIndex = glGetAttribLocation(shaderProgram, "position");
+ 	glEnableVertexAttribArray(positionIndex);
+ 	glVertexAttribPointer(positionIndex,2,GL_FLOAT,GL_FALSE,0,0);
+ 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+ 	glBindVertexArray(0);
+}
+
+
+GLuint createShaderProgram(GLchar* vSource, const GLint vLength, GLchar* fSource, const GLint fLength)
+{
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+ 	glShaderSource(vShader, 1, (const GLchar**)(&vSource), &vLength);
+ 	glCompileShader(vShader);
+
+ 	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+ 	glShaderSource(fShader, 1, (const GLchar**)(&fSource), &fLength);
+ 	glCompileShader(fShader);
+
+ 	GLuint program = glCreateProgram();
+ 	glAttachShader(program, vShader);
+ 	glAttachShader(program, fShader);
+ 	glLinkProgram(program);
+ 	return program;
+
+}
+
 
 
 
@@ -136,53 +166,23 @@ int main(void)
  	unsigned int indices[] = {0, 1, 2};
 
     GLuint vbo;
- 	createVBO(positions, 3,2,vbo);
- 	//glGenBuffers(1,vbo);
- 	//glBindBuffer(GL_ARRAY_BUFFER, vbo);
- 	//glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
- 	//glBindBuffer(GL_ARRAY_BUFFER, 0);
-
     GLuint ibo;
- 	createIBO(indices,3, ibo);
- 	//glGenBuffers(1,&ibo);
- 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
- 	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
- 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
- 	GLint vLength, fLength;
+    GLuint vao;
+    GLint vLength, fLength;
  	GLchar * vSource = new GLchar[10000];
  	GLchar * fSource = new GLchar[10000];
+ 	GLuint program;
+
+
+ 	createVBO(positions, 3,2,vbo);
+ 	createIBO(indices,3, ibo);
 
  	file_contents("../shaders/myOwnGLSLProg.v.glsl", &vLength, vSource);
  	file_contents("../shaders/myOwnGLSLProg.f.glsl", &fLength, fSource);
 
-	cout << vLength << endl;
-	cout << fLength << endl;
+    program = createShaderProgram(vSource, vLength, fSource, fLength);
 
- 	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
- 	glShaderSource(vShader, 1, (const GLchar**)(&vSource), &vLength);
- 	glCompileShader(vShader);
- 	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
- 	glShaderSource(fShader, 1, (const GLchar**)(&fSource), &fLength);
- 	glCompileShader(fShader);
- 	std::cout << "Yes we can!!!" << std::endl;
-
- 	GLuint program = glCreateProgram();
- 	glAttachShader(program, vShader);
- 	glAttachShader(program, fShader);
- 	glLinkProgram(program);
-
-	GLuint vao;
- 	glGenVertexArrays(1,&vao);
- 	glBindVertexArray( vao);
- 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
- 	GLint positionIndex = glGetAttribLocation(program, "position");
- 	glEnableVertexAttribArray(positionIndex);
- 	glVertexAttribPointer(positionIndex,2,GL_FLOAT,GL_FALSE,0,0);
- 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
- 	glBindVertexArray(0);
- 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
+    createVAO(program,ibo, vbo, vao);
 
     GLFWwindow* window = glfwGetCurrentContext();
 
