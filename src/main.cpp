9c6c51711eac7301a27f0bf79bc1dbfbf60ae90d
Pierre Gabon <pierre.gabon@ecole.ensicaen.fr>
2017-03-22 18:56:34 +0100

    class 3DObject

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,17 @@ int main(void)
  	OGL::init();
 
  	float positions[] = {0.2f,0.2f,0.2f,1.0f,1.0f,1.0f};
+ 	float pos2[] = {0.0f , 0.0f, 1.0f, -1.0f, 0.0f,-1.0f};
  	unsigned int indices[] = {0, 1, 2};
+ 	//unsigned int ind2[] = {0, 1, 2};
 
  	Mesh test(positions, indices);
+ 	Mesh test2( pos2, indices);
 
     GLuint vbo;
+    GLuint vbo2;
     GLuint ibo;
+    GLuint vao2;
     GLuint vao;
     GLint vLength, fLength;
  	GLchar * vSource = new GLchar[10000];
@@ -19,7 +24,9 @@ int main(void)
  	GLuint program;
 
  	OGL::createVBO(test.getPositions(), 3,2,vbo);
+ 	OGL::createVBO(test2.getPositions(), 3,2,vbo2);
  	OGL::createIBO(test.getIndices(),3, ibo);
+ 	OGL::createIBO(test2.getIndices(),3, ibo);
 
  	OGL::file_contents("../shaders/myOwnGLSLProg.v.glsl", &vLength, vSource);
  	OGL::file_contents("../shaders/myOwnGLSLProg.f.glsl", &fLength, fSource);
@@ -27,12 +34,15 @@ int main(void)
     program = OGL::createShaderProgram(vSource, vLength, fSource, fLength);
 
     OGL::createVAO(program,ibo, vbo, vao);
+    OGL::createVAO(program,ibo, vbo2, vao2);
 
     GLFWwindow* window = glfwGetCurrentContext();
 
  	while (!glfwWindowShouldClose (window))
  	{
- 		OGL::renderFrame(program, vao);
+        OGL::renderFrame(program, vao);
+ 		//OGL::renderFrame();
+
 	}
 
 	glfwTerminate();
