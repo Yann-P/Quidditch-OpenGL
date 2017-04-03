2405d9acd90dc6cf20172b01903543136f8f17a7
Bouvier <pierrick.bouvier.aubrege@hotmail.fr>
2017-04-03 10:40:00 +0200

    ajout blend et shader au git

---
+++ shaders/character.f.glsl
@@ -0,0 +1,13 @@
+#version 410
+
+uniform sampler2D textureBalai;
+in vec3 Normal;
+in vec2 Uv;
+out vec4 out_color;
+
+
+
+void main()
+{
+	out_color=vec4(Normal, 1); //texture(textureBalai, Uv); //vec4(1,1,0,1);
+}
---
+++ shaders/character.v.glsl
@@ -0,0 +1,19 @@
+#version 410
+
+in vec3 position;
+in vec3 normal;
+in vec2 uv;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+out vec2 Uv;
+out vec3 Normal;
+
+void main()
+{
+	gl_Position = projection * view * model * vec4(position, 1.0);
+	Uv=uv;
+	Normal=normal;
+}
